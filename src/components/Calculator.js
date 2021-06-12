174c0deecc832ed01f5b7755eb34ccd09d051e8e
Jose Estrada <j.estrada49@icloud.com>
2021-06-12 13:51:18 -0700

    User stories fulfilled

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,27 +2,140 @@ import React, { Component } from 'react'
 import CalculatorDisplay from './calculator/CalculatorDisplay';
 
 export default class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      current: '0',
+      total: '',
+    }
+
+    this.handleButton = this.handleButton.bind(this);
+  }
+
+  // Reset the display's values 
+  handleClear() {
+    this.setState({
+      current: '0',
+      total: '',
+    })
+  }
+
+  // Compute the total
+  handleComputation() {
+    const { current, total } = this.state;
+    if(!/[+\-*/]/.test(current[current.length -1])) {
+      this.setState({
+        current:  Math.round(eval(`${total}${current}`) * 10000) / 10000,
+        total: '',
+      })
+    }
+  }
+
+  // Insert the operator
+  handleOperator(operator) {
+    const { current, total } = this.state;
+    if(/[+\-*/]/.test(current)) {
+      if(/[+\-*/]/.test(current[current.length -1])) {
+        if(operator !== '-') {
+          this.setState({
+            current: operator,
+          })
+        } else if(!/-/.test(current)) {
+          this.setState({
+            current: `${current}${operator}`
+          })
+        }
+      } else {
+        this.setState({
+          current: operator,
+          total: Math.round(eval(`${total}${current}`) * 10000) / 10000,
+        })
+      }
+    } else {
+      this.setState({
+        current: operator,
+        total: current,
+      })
+    }
+  }
+
+  handleDigit(digit) {
+    const { current } = this.state;
+    this.setState({
+      current: current !== '0' ? `${current}${digit}` : digit,
+    })
+  }
+
+  handleDecimal() {
+    const { current } = this.state;
+    if(!/\./.test(current)) {
+      this.setState({
+        current: `${current}.`
+      })
+    }
+  }
+
+  handleButton(e) {
+    const {textContent: value} = e.target;
+
+    switch(value) {
+      case 'AC':
+        this.handleClear();
+        break;
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        this.handleDigit(value);
+        break;
+      case '+':
+      case '-':
+      case '*':
+      case '/':
+        this.handleOperator(value);
+        break;
+      case '=':
+        this.handleComputation();
+        break;
+      case '.':
+        this.handleDecimal();
+        break;
+      default:
+        console.error('Technically, this should never happen.');
+    }
+  }
+
   render() {
+    const { current, total } = this.state;
+    const { handleButton } = this;
+
     return (
       <div id='calculator'>
-        <CalculatorDisplay />
-        <div id='equals' className='item'>=</div>
-        <div id='seven' className='item'>7</div>
-        <div id='eight' className='item'>8</div>
-        <div id='nine' className='item'>9</div>
-        <div id='four' className='item'>4</div>
-        <div id='five' className='item'>5</div>
-        <div id='six' className='item'>6</div>
-        <div id='one' className='item'>1</div>
-        <div id='two' className='item'>2</div>
-        <div id='three' className='item'>3</div>
-        <div id='zero' className='item'>0</div>
-        <div id='add' className='item operator'>+</div>
-        <div id='subtract' className='item operator'>-</div>
-        <div id='multiply' className='item operator'>x</div>
-        <div id='divide' className='item operator'>/</div>
-        <div id='decimal' className='item'>.</div>
-        <div id='clear' className='item'>AC</div>
+        <CalculatorDisplay current={current} total={total} />
+        <div id='equals' className='item' onClick={handleButton}>=</div>
+        <div id='seven' className='item' onClick={handleButton}>7</div>
+        <div id='eight' className='item' onClick={handleButton}>8</div>
+        <div id='nine' className='item' onClick={handleButton}>9</div>
+        <div id='four' className='item' onClick={handleButton}>4</div>
+        <div id='five' className='item' onClick={handleButton}>5</div>
+        <div id='six' className='item' onClick={handleButton}>6</div>
+        <div id='one' className='item' onClick={handleButton}>1</div>
+        <div id='two' className='item' onClick={handleButton}>2</div>
+        <div id='three' className='item' onClick={handleButton}>3</div>
+        <div id='zero' className='item' onClick={handleButton}>0</div>
+        <div id='add' className='item operator' onClick={handleButton}>+</div>
+        <div id='subtract' className='item operator' onClick={handleButton}>-</div>
+        <div id='multiply' className='item operator' onClick={handleButton}>*</div>
+        <div id='divide' className='item operator' onClick={handleButton}>/</div>
+        <div id='decimal' className='item' onClick={handleButton}>.</div>
+        <div id='clear' className='item' onClick={handleButton}>AC</div>
       </div>
     )
   }
--- src/components/calculator/CalculatorDisplay.js
+++ src/components/calculator/CalculatorDisplay.js
@@ -4,8 +4,8 @@ export default class CalculatorDisplay extends Component {
   render() {
     return (
       <div id='CalculatorDisplay' className='item'>
-        <div id='formula'>Formula</div>
-        <div id='display'>Current Value</div>
+        <div id='formula'>{this.props.total}</div>
+        <div id='display'>{this.props.current}</div>
       </div>
     )
   }
